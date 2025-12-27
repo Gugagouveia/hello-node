a394511528eefe3864e266bc84ef9de1af0b2c59
Gustavo Gouveia <gustavo.gouveia.ext@aramisinc.com.br>
2025-12-27 16:56:23 -0300

    feat:hello world node app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 80;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
